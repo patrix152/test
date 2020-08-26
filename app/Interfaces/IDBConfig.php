fac9ca22e2be47ff8026949f66d3f1448634aee2
patrix152 <34523222+patrix152@users.noreply.github.com>
2020-08-26 22:31:24 +0200

    DBConnector

---
+++ app/Interfaces/IDBConfig.php
@@ -0,0 +1,8 @@
+<?php
+interface IDBConfig {
+    const host = '127.0.0.1';
+    const dbname = 'test';
+    const user = 'test';
+    const pass = '';
+}
+?>
---
+++ app/Model/DBConfig.php
@@ -0,0 +1,22 @@
+<?php
+require 'C:\xampp\htdocs\test\test\app\Interfaces\IDBConfig.php';
+Class DBConfig implements IDBConfig  {
+    private $host = IDBConfig::host;
+    private $dbname = IDBConfig::dbname;
+    private $user = IDBConfig::user;
+    private $pass = IDBConfig::pass;    
+    function __construct(){
+        $this->connect();
+    }
+    function connect(){
+      try {
+        $connect = new PDO ('mysql:host='.$this->host.';dbname='.$this->dbname.','.$this->user, $this->pass);        
+    }
+      catch (PDOException $w){
+          print $w->getMessage();
+
+      }
+    }
+}
+
+?>
