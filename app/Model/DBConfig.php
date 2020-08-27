d0f817094768c7a3866d8a94bfb13686286b1706
patrix152 <34523222+patrix152@users.noreply.github.com>
2020-08-27 10:49:45 +0200

    Update Operations

--- app/Model/DBConfig.php
+++ app/Model/DBConfig.php
@@ -4,11 +4,11 @@ Class DBConfig implements IDBConfig  {
     private $host = IDBConfig::host;
     private $dbname = IDBConfig::dbname;
     private $user = IDBConfig::user;
-    private $pass = IDBConfig::pass;    
-    
-    function connect(){
-      try {
-        new PDO ("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);       
+    private $pass = IDBConfig::pass; 
+    public $pdo;   
+    function __construct(){
+    try {
+        $this->pdo = new PDO ("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);       
       }
       catch (PDOException $w){
           print $w->getMessage();
--- app/Model/Operations.php
+++ app/Model/Operations.php
@@ -1,4 +1,5 @@
 <?php
+require 'DBConfig.php';
 Class Operations {
     public $table;
     public $sql;
@@ -6,24 +7,26 @@ Class Operations {
     public $value;
     function getAllRecords($table){
         $this->table = $table;
-        $sql = "SELECT * FROM $this->table";   
+        $sql = "SELECT * FROM $this->table";  
+        return $this->DoOperation($sql); 
     }
     function getRecord($id, $table){
         $this->id = $id;
         $this->table = $table;
         $sql = "SELECT * FROM $this->table FROM $this->id";
+        return $this->DoOperation($sql);
     }
     function insertRecord($value, $table){
         $this->value = $value;
         $this->table = $table;
         $sql = "INSERT INTO $this->table (record1) VALUES $this->value";
+        return $this->DoOperation($sql);
     }
     function DoOperation($sql){
         $this->sql = $sql;
         $conObj = new DBConfig();
+        $conObj->pdo->prepare($this->sql);
     }
-   
-
-
 
 }
+
